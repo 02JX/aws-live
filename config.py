7e4bd188383e1ebc33f8f4a09135af3c1ed99121
JX <98585275+02JX@users.noreply.github.com>
2023-09-10 21:39:45 +0800

    Update config.py

--- config.py
+++ config.py
@@ -1,7 +1,7 @@
-customhost = "employee.cspnc5ouklrd.us-east-1.rds.amazonaws.com"
+customhost = "employee.cjzspdft33rp.us-east-1.rds.amazonaws.com"
 customuser = "aws_user"
 custompass = "Bait3273"
 customdb = "employee"
-custombucket = "lowchoonkeat-employee"
+custombucket = "leejiaxuan-employee"
 customregion = "us-east-1"
 
