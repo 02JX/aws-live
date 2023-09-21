a2b06f9b65edd882f5b0bf34def0dacecc1516ac
JX <sammiexuan@gmail.com>
2023-09-22 01:12:24 +0800

    Update EmpApp.py

--- EmpApp.py
+++ EmpApp.py
@@ -88,7 +88,6 @@ def home():
 
 #------------------------------------------------------------------------------Student Sign Up
 students = {}
-<<<<<<< HEAD
 
 @app.route('/')
 def index():
@@ -118,91 +117,6 @@ def signup():
     insert_sql = "INSERT INTO employee VALUES (%s, %s, %s, %s, %s)"
     cursor = db_conn.cursor()
 
-    if emp_image_file.filename == "":
-        return "Please select a file"
-
-    try:
-
-        cursor.execute(insert_sql, (student_id, first_name, last_name, password, confirm_password))
-        db_conn.commit()
-        std_name = "" + first_name + " " + last_name
-        # Uplaod image file in S3 #
-        
-
-        # try:
-        #     print("Data inserted in MySQL RDS... uploading image to S3...")
-        #     s3.Bucket(custombucket).put_object(Key=emp_image_file_name_in_s3, Body=emp_image_file)
-        #     bucket_location = boto3.client('s3').get_bucket_location(Bucket=custombucket)
-        #     s3_location = (bucket_location['LocationConstraint'])
-
-        #     if s3_location is None:
-        #         s3_location = ''
-        #     else:
-        #         s3_location = '-' + s3_location
-
-        #     object_url = "https://s3{0}.amazonaws.com/{1}/{2}".format(
-        #         s3_location,
-        #         custombucket,
-        #         emp_image_file_name_in_s3)
-
-        except Exception as e:
-            return str(e)
-
-    finally:
-        cursor.close()
-
-    print("all modification done...")
-    return render_template('StudLogin.html')
-=======
->>>>>>> ad11b3f7aecc8f66b4f952fde60ab782b0828e5a
-
-@app.route('/')
-def index():
-    return render_template('StudentSignUp.html')
-
-<<<<<<< HEAD
-#------------------------------------------------------------signin
-    @app.route('/studlogin', methods=['POST', 'GET'])
-    def signin():
-    if request.method == 'POST':
-        student_id = request.form.get('std_lg_id')
-        password = request.form.get('std_lg_pass')
-
-        # Check if the student exists in the dictionary (for demonstration purposes)
-        if student_id in students and students[student_id]['password'] == password:
-            return f"Welcome, Student with ID {student_id}!"
-        else:
-            return "Invalid student ID or password."
-
-    return render_template('StudLogin.html')
-
-=======
-@app.route('/studentsignup', methods=['POST'])
-def signup():
-    student_id = request.form.get('std_id')
-    first_name = request.form.get('std_first_name')
-    last_name = request.form.get('std_last_name')
-    password = request.form.get('std_pass')
-    confirm_password = request.form.get('confirm_std_pass')
-
-    # Check if passwords match
-    if password != confirm_password:
-        return "Password confirmation does not match."
-
-    # Store student data in the dictionary
-    students[student_id] = {
-        'first_name': first_name,
-        'last_name': last_name,
-        'password': password
-    }
-
-    return f"Student with ID {student_id} signed up successfully."
-    
-    insert_sql = "INSERT INTO employee VALUES (%s, %s, %s, %s, %s)"
-    cursor = db_conn.cursor()
-
-    if emp_image_file.filename == "":
-        return "Please select a file"
 
     try:
 
@@ -237,6 +151,7 @@ def signup():
     print("all modification done...")
     return render_template('StudLogin.html')
 
+
 #------------------------------------------------------------signin
     # @app.route('/studlogin', methods=['POST', 'GET'])
     # def signin():
@@ -251,16 +166,6 @@ def signup():
     #         return "Invalid student ID or password."
 
     # return render_template('StudLogin.html')
-
->>>>>>> ad11b3f7aecc8f66b4f952fde60ab782b0828e5a
-#-------------------------------------------------------------------------------------------------------
-
-
-
-
-
-
-
 
 
 
