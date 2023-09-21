311dd4b317897d928a8b0b2cb84c4bd515df9bd5
JX <98585275+02JX@users.noreply.github.com>
2023-09-21 18:28:49 +0800

    update config

--- config.py
+++ config.py
@@ -1,7 +1,7 @@
-customhost = "assignment.cjzspdft33rp.us-east-1.rds.amazonaws.com"
+customhost = "ccassignment.cjzspdft33rp.us-east-1.rds.amazonaws.com"
 customuser = "aws_user"
 custompass = "Bait3273"
 customdb = "assignment"
-custombucket = "leejiaxuan-assignment"
+custombucket = "leejiaxuan-ccassignment"
 customregion = "us-east-1"
 
