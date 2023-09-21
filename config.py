781f8caf8d036fcaa54104df7c550902d6b307a8
JX <98585275+02JX@users.noreply.github.com>
2023-09-21 17:59:56 +0800

    Add files via upload

--- config.py
+++ config.py
@@ -1,7 +1,7 @@
-customhost = "employee.cjzspdft33rp.us-east-1.rds.amazonaws.com"
+customhost = "assignment.cjzspdft33rp.us-east-1.rds.amazonaws.com"
 customuser = "aws_user"
 custompass = "Bait3273"
-customdb = "employee"
-custombucket = "leejiaxuan-employee"
+customdb = "assignment"
+custombucket = "leejiaxuan-assignment"
 customregion = "us-east-1"
 
