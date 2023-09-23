3ad97cd9cab341948723a3386dd75ee37a6647a4
James Q <tranqh-wm21@student.tarc.edu.my>
2023-09-24 02:24:12 +0800

    Update EmpApp.py

--- EmpApp.py
+++ EmpApp.py
@@ -161,7 +161,7 @@ def student_signin():
 
     if student_id and password:
         for row in dbPassword:
-            if row[0] == student_id and row[3] == password:
+            if row[0] == student_id and row[1] == password:
                 return("Login Success!")
             else:
                 return("Wrong details 1")
